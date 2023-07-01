34a92b5d89723c8e612c529d8d536b0dc4beac43
tquoctuan97 <tquoctuan97@gmail.com>
2023-07-02 02:28:18 +0700

    feat: create product schema and crud products on database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './modules/products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.CONNECTIONSTRING),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -10,7 +10,7 @@ import {
 import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
-import { Product } from 'src/models/product.model';
+import { Product } from 'src/modules/products/product.model';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
@@ -34,15 +34,12 @@ export class ProductsController {
   @ApiCreatedResponse({
     type: Product,
   })
-  create(@Body() createProductDto: CreateProductDto): Product {
+  create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Put(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateProductDto: UpdateProductDto,
-  ): Product {
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
