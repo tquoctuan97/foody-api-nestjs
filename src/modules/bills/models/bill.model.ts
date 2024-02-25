ed175e2f232ed5c609323ebf370be3270d1b19d2
tquoctuan97 <tquoctuan97@gmail.com>
2024-02-25 21:41:14 +0700

    feat: filter by billDate

--- src/modules/bills/models/bill.model.ts
+++ src/modules/bills/models/bill.model.ts
@@ -1,3 +1,9 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { PaginationParams } from 'src/common/pagination/pagination.model';
 
-export class BillParams extends PaginationParams {}
+export class BillParams extends PaginationParams {
+  @ApiPropertyOptional({
+    description: '2024-01-01T00:00:00.000Z',
+  })
+  billDate?: string;
+}
