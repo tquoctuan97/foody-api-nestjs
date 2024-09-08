9bf47ec587de18defc319acb585fd87e2eaf78c7
tquoctuan97 <tquoctuan97@gmail.com>
2024-09-08 17:11:24 +0700

    fix(#17): decode URI seach parameter

--- src/modules/customers/customers.service.ts
+++ src/modules/customers/customers.service.ts
@@ -25,8 +25,10 @@ export class CustomersService {
     const currentPage = parseInt(query?.page) || 1;
     const pageSize = parseInt(query?.pageSize) || 10;
 
+    const decodedSearch = decodeURI(query?.search || '');
+
     const queryCustomer: FilterQuery<Customer> = {
-      slug: new RegExp(convertVietnameseToSlug(query?.search) || '', 'i'),
+      slug: new RegExp(convertVietnameseToSlug(decodedSearch) || '', 'i'),
       ...(query?.isDeleted
         ? { deletedAt: { $ne: null } }
         : { deletedAt: null }),
