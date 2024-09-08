cca24069628566b325cdb07b10b770acea6092e1
tquoctuan97 <tquoctuan97@gmail.com>
2024-09-08 22:06:39 +0700

    feat(#19): add adjustment list to bill

--- src/modules/bills/dto/create-bill.dto.ts
+++ src/modules/bills/dto/create-bill.dto.ts
@@ -1,51 +1,88 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsUrl, Min, MinLength } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+import { IsObjectId } from 'src/common/is-object-id/is-object-id.decorator';
 
 export class BillItem {
   @ApiProperty({
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
   @ApiProperty({
     required: true,
   })
+  @IsNumber()
   price: number;
 
   @ApiProperty({
     required: true,
   })
+  @IsNumber()
+  @Min(1)
   quantity: number;
 
   @ApiProperty({
     required: true,
   })
+  @IsNumber()
   total: number;
 }
 
-export class CreateBillDto {
+export class AdjustmentItem {
   @ApiProperty({
     required: true,
-    type: Date,
   })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({ required: true, enum: ['add', 'subtract'] })
+  @IsEnum(['add', 'subtract'], { message: 'Type must be "add" or "subtract"' })
+  type: 'add' | 'subtract';
+
+  @ApiProperty({ required: true })
+  @IsNumber()
+  amount: number;
+}
+
+export class CreateBillDto {
+  @ApiProperty({ required: true, type: Date })
+  @IsDate()
+  @Type(() => Date)
   billDate: Date;
 
   @ApiProperty({
     required: true,
     type: String,
   })
+  @IsNotEmpty()
+  @IsString()
   customerName: string;
 
   @ApiProperty({
     required: false,
     type: String,
   })
-  customerId: string;
+  @IsOptional()
+  @IsString()
+  @IsObjectId()
+  customerId?: string;
 
-  @ApiProperty({
-    required: true,
-    type: [BillItem],
-  })
+  @ApiProperty({ required: true, type: [BillItem] })
+  @ValidateNested({ each: true })
+  @Type(() => BillItem)
   billList: BillItem[];
 
   @ApiProperty({
@@ -58,19 +95,30 @@ export class CreateBillDto {
     required: false,
     default: null,
     type: Number,
+    deprecated: true,
   })
-  debt: number | null;
+  @IsOptional()
+  @IsNumber()
+  debt?: number | null;
 
   @ApiProperty({
     required: false,
     default: null,
     type: Number,
+    deprecated: true,
   })
-  prePay: number | null;
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  prePay?: number | null;
 
-  @ApiProperty({
-    required: true,
-    type: Number,
-  })
+  @ApiProperty({ required: false, type: [AdjustmentItem] })
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => AdjustmentItem)
+  adjustmentList: AdjustmentItem[];
+
+  @ApiProperty({ required: true, type: Number })
+  @IsNumber()
   finalResult: number;
 }
